4eafa512a51f2b282c684f553458893ef05fe342
Tamilarasu21 <tamilarasuat1998@gmail.com>
2021-01-27 21:55:56 +0530

    timeline added and images optimization

--- demo/assets/js/main.js
+++ demo/assets/js/main.js
@@ -218,3 +218,9 @@
   });
 
 })(jQuery);
+
+// showing file name in input file 
+$(".custom-file-input").on("change", function() {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
